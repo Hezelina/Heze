bb9d0eea5112b4dec8d9c950fd6a14a2ea2570ac
Hezelina <65491952+Hezelina@users.noreply.github.com>
2020-05-18 22:41:34 +0700

    Add files via upload

---
+++ week2.13.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+int main() {
+
+	float a, b, result, result2;
+
+	std::cout << "Enter your prices: " << std::endl;
+	std::cin >> a >> b;
+
+	result = (a * 0.3) + (b * 0.4);      //a
+
+	result2 = ((a * 1.8) + (b * 2)) * 3;    //b
+
+	std::cout << "Result:" << result << std::endl
+		<< result2 << std::endl;
+
+	std::getchar();
+	std::getchar();
+
+	return 0;
+}
