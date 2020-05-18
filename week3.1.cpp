9b195b33d39d18816c5c39073113bb5ff69c3864
Hezelina <65491952+Hezelina@users.noreply.github.com>
2020-05-18 22:46:24 +0700

    Add files via upload

---
+++ week3.1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+float n;
+
+int main()
+{
+	std::cout << "Enter n: ";
+	std::cin >> n;
+	for (int i = 1; i < n; i++) {
+		for (int j = 0; j < i; j++)
+			std::cout << 0;
+		std::cout << std::endl;
+	}
+
+	std::cout << std::endl;
+
+	std::getchar();
+	std::getchar();
+	return 0;
+}
