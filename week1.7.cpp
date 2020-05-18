119a0c9ca1b47eb05b5addb1e5867ee9a099e507
Hezelina <65491952+Hezelina@users.noreply.github.com>
2020-05-18 22:34:03 +0700

    Add files via upload

---
+++ week1.7.cpp
@@ -0,0 +1,22 @@
+﻿
+#include <iostream>
+
+int main() {
+
+	float a, cube, square;
+
+
+	std::cout << "Please, insert the number: " << std::endl;
+	std::cin >> a;
+
+	square = a * a;
+	cube = square * a;
+
+	std::cout << square << std::endl
+		<< cube << std::endl;
+
+	std::getchar();
+	std::getchar();
+
+	return 0;
+}
