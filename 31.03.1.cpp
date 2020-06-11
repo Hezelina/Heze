36bdbdfec3974b075c458220a5e1956f807539bf
Hezelina <65491952+Hezelina@users.noreply.github.com>
2020-06-12 00:34:49 +0700

    Add files via upload

---
+++ 31.03.1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <fstream>
+#include <iomanip> 
+#include <math.h>
+
+int main() {
+
+	const int max = 25;
+	int array[max];
+
+	srand(300);
+
+	for (int i = -10; i <= max; i++) {
+		array[i] = rand() % 26 - 10;
+		std::cout << array[i] << std::endl;
+	}
+
+	getchar();
+	getchar();
+
+	return 0;
+}
