65219efd711460ba9f68a17ca1be189eb2d73c34
Hezelina <65491952+Hezelina@users.noreply.github.com>
2020-07-03 17:32:50 +0300

    Add files via upload

---
+++ week13.4z.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+	fstream f;
+	f.open("Z4.txt", ios::out | ios::trunc);
+
+	string str("hahahhahah       lol      kek ");
+	int k = 0;
+	cout << str << endl;
+	f << str << endl;
+
+	while (k != str.size())
+	{
+		if (str[k] == ' ' && str[k + 1] == ' ')
+			str.erase(k, 1);
+		else k++;
+	}
+	cout << str << endl;
+	f << str << endl;
+	f.close();
+	system("pause");
+}
